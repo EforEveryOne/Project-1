9cde285251d5ee47e95ed8dd1904bfc5f40bda1c
EforEveryOne <31865908+EforEveryOne@users.noreply.github.com>
2017-09-18 14:42:43 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    float time;
+    bool printedMessage;
+
+    // Use this for initialization
+    void Start () {
+	   print ("The game has begun!");
+
+       printedMessage = false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+    if (Time.time > 3 && printedMessage == false) {
+		//stuff the happens if true time.time is a floating point numbers, greater or less than
+		print ("It has been three seconds!");
+		printedMessage = true;
+	         }
+		}
+}
